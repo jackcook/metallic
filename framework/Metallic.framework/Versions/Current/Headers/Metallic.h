07c1a4ea227c9f1e7f7a12bb4f95ff9f3b81333d
Jack Cook <hello@jackcook.com>
2023-02-03 19:00:36 -0800

    Update .gitignore and add framework to git

---
+++ framework/Metallic.framework/Versions/Current/Headers/Metallic.h
@@ -0,0 +1,19 @@
+//
+//  Metallic.h
+//  Metallic
+//
+//  Created by Jack Cook on 8/8/19.
+//  Copyright © 2019 Jack Cook. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Metallic.
+FOUNDATION_EXPORT float MetallicVersionNumber;
+
+//! Project version string for Metallic.
+FOUNDATION_EXPORT const unsigned char MetallicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Metallic/PublicHeader.h>
+
+
