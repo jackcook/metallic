6c559a359acb160c82477ffc8dd05e123c1c6dd2
Jack Cook <hello@jackcook.com>
2023-02-03 20:30:58 -0800

    Update description

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from setuptools import setup
 
 setup(name="metallic",
       version="0.1",
-      description="The funniest joke in the world",
+      description="Metal-accelerated framework for scientific computing",
       url="http://github.com/jackcook/metallic",
       author="Jack Cook",
-      author_email="cookj@mit.edu",
+      author_email="hello@jackcook.com",
       license="MIT",
       packages=["metallic"],
-      install_requires=[
-            "pyobjc"
-      ],
+      install_requires=["pyobjc"],
       zip_safe=False)
