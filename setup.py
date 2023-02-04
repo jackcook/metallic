fa88f85331987f600b4f4e8a8b82a7d2ef7c8bf2
Jack Cook <hello@jackcook.com>
2023-02-03 19:00:04 -0800

    Add pyobjc dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='metallic',
-      version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/jackcook/metallic',
-      author='Jack Cook',
-      author_email='cookj@mit.edu',
-      license='MIT',
-      packages=['metallic'],
+setup(name="metallic",
+      version="0.1",
+      description="The funniest joke in the world",
+      url="http://github.com/jackcook/metallic",
+      author="Jack Cook",
+      author_email="cookj@mit.edu",
+      license="MIT",
+      packages=["metallic"],
+      install_requires=[
+            "pyobjc"
+      ],
       zip_safe=False)
